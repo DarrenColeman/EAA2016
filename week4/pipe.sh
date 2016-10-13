13fc2b37a7a753748c73fa913806497b004d8a5c
John Burns <jzburns@gmail.com>
2016-10-13 10:50:30 +0100

    pipe

---
+++ week4/pipe.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ps -ef | grep mysql
+
+#ps -ef prints snapshot of current processes running on the system
+
+#grep - searches for string related to the text after, in this example mysql.
